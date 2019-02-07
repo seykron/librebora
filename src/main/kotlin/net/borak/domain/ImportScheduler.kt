8ac32ef81f8ad5a5110ece9b17c3acf11cc94826
seykron <foo@bar>
2019-02-07 06:46:41 -0300

    Removes unused classes

--- src/main/kotlin/net/borak/domain/ImportScheduler.kt
+++ src/main/kotlin/net/borak/domain/ImportScheduler.kt
@@ -7,10 +7,10 @@ import org.joda.time.DateTime
 class ImportScheduler(private val importService: ImportService) {
 
     fun start() = GlobalScope.launch {
-        importService.import(
+        /*importService.import(
             sectionName =  "segunda",
             startDate = DateTime.now().minusDays(4),
             endDate = DateTime.now().minusDays(3)
-        )
+        )*/
     }
 }
--- src/main/kotlin/net/borak/support/http/RequestHandler.kt
+++
@@ -1,5 +0,0 @@
-package net.borak.support.http
-
-interface RequestHandler<RequestType, ResponseType> {
-    fun handle(request: RequestType): ResponseType
-}
